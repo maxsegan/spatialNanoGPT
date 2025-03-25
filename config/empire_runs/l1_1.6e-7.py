d20c2fb3c53d57b6ab447f9e94180cc8072dd1ef
Max Segan <mms2305@columbia.edu>
2025-03-25 12:46:12 -0400

    all the L1 configs

---
+++ config/empire_runs/l1_1.6e-7.py
@@ -0,0 +1,29 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+wandb_log = True
+wandb_project = 'owt'
+wandb_run_name='gpt2-baseline'
+
+# these make the total batch size be ~0.5M
+# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
+batch_size = 12
+block_size = 1024
+gradient_accumulation_steps = 5 * 8
+
+# this makes total number of tokens be 300B / 6
+max_iters = 100000
+lr_decay_iters = 100000
+
+# eval stuff
+eval_interval = 1000
+eval_iters = 200
+log_interval = 10
+
+# weight decay
+weight_decay = 1e-1
+
+# regularization
+spatial_cost_scale = 0.0
+l1_scale = 1.6e-7
